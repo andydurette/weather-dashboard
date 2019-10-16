93718fe155dd5d68eca84103e830f94c5773d489
Andy Durette <duretteandy@gmail.com>
2019-10-16 17:58:27 -0400

    Security protocol fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,7 +9,7 @@ var weatherUpdate = function(cityName, searched){
 
 
   // First AJAX call gets the city name provided to the function
-  $.ajax({url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, success: function(result){
+  $.ajax({url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, success: function(result){
     
   // Checks if was sent from searchfield
   if(searched === true){
@@ -18,7 +18,7 @@ var weatherUpdate = function(cityName, searched){
       searchedCities.push($("#search input").val());
       localStorage.setItem("citysearch", JSON.stringify(searchedCities));
     }
-    
+
     localStorage.setItem("lastCitySearch", $("#search input").val());
    }
 
@@ -34,12 +34,12 @@ var weatherUpdate = function(cityName, searched){
       // Empties current day information
       $("#currentDay").html("");
       // Sets current day information
-      $("#currentDay").append(`<div class="blockHeading"><h2>${result.city.name} ( ${getDate(0)} )</h2><img src="http://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png" alt="${result.list[0].weather[0].description}" width='50' height='50'>`);
+      $("#currentDay").append(`<div class="blockHeading"><h2>${result.city.name} ( ${getDate(0)} )</h2><img src="https://openweathermap.org/img/w/${result.list[0].weather[0].icon}.png" alt="${result.list[0].weather[0].description}" width='50' height='50'>`);
       $("#currentDay").append(`<p class="humidity">Temperature: ${result.list[0].main.temp} °C</p>`);
       $("#currentDay").append(`<p class="humidity"> Humidity: ${result.list[0].main.humidity} %</p>`);
       $("#currentDay").append(`<p class="uv">Wind Speed:  ${mph(result.list[0].wind.speed)} MPH</p>`);
       // Third AJAX call uses the co-ordinate data from the second to call the uv data
-      $.ajax({url: "http://api.openweathermap.org/data/2.5/uvi?appid=428bbab3989b31eb5f6dd40e0559cbeb&lat=" + result.city.coord.lat + "&lon=" + result.city.coord.lon, success: function(result){
+      $.ajax({url: "https://api.openweathermap.org/data/2.5/uvi?appid=428bbab3989b31eb5f6dd40e0559cbeb&lat=" + result.city.coord.lat + "&lon=" + result.city.coord.lon, success: function(result){
         $("#currentDay").append(`<p class="uv">UV Index: <span>${result.value}</span></p>` );
       }});
   
@@ -51,7 +51,7 @@ var weatherUpdate = function(cityName, searched){
         let forcastBlock = function(i){
           return('<div>' +
           '<p class="date">' + getDate(i) + '</p>' +  
-          `<img src="http://openweathermap.org/img/w/${result.list[i].weather[0].icon}.png" alt="${result.list[i].weather[0].description}" "width='50' height='50'>` +
+          `<img src="https://openweathermap.org/img/w/${result.list[i].weather[0].icon}.png" alt="${result.list[i].weather[0].description}" "width='50' height='50'>` +
           `<p class="temperature">Temp: ${result.list[i].main.temp}&nbsp;°C</p>` +
           `<p class="humidity">Humidity: ${result.list[i].main.humidity}&nbsp;%"</p>` +
           '</div>');
